cee6b727f9ada8e0b75314804bac584b8040525b
emacosta95 <emanuelecosta95@hotmail.com>
2025-04-22 20:03:06 +0200

    implement the deep learning dft training code

---
+++ job.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+nohup python ./dft_for_ising/train.py  --hidden_channel 40 40 40 40   --kernel_size 5 5 --padding 2 2  --model_name=model_test --data_path=data/datasets/dataset_for_training_ndata_10000_test0.npz --model_type=REDENTnopooling2D --pooling_size=1 --epochs=3000 > output_train.txt &
+
