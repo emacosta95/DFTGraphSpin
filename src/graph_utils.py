66c20e7a67f845f44c4b16ab6064392ce765a6d5
emacosta95 <emanuelecosta95@hotmail.com>
2025-05-03 16:25:47 +0200

    fix the parameter in the S-K spin gass configuration

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -20,6 +20,20 @@ def create_random_coupling_graph(num_nodes, edge_prob=0.5,sigma:float=0.5, seed=
 
     return graph
 
+def create_fully_connected_sk_graph(num_nodes, sigma=0.5, seed=None):
+    np.random.seed(seed)
+    
+    graph = nx.Graph()
+    graph.add_nodes_from(range(num_nodes))
+
+    for i in range(num_nodes):
+        for j in range(i+1, num_nodes):
+            coupling_ij = np.random.normal(0, sigma)
+            graph.add_edge(i, j, weight=coupling_ij)
+
+    return graph
+
+
 def generate_spin_basis(n_sites):
     bin_states = np.array(list(itertools.product([0, 1], repeat=n_sites)))
     return 2 * bin_states - 1  # Map 0 -> -1, 1 -> +1
